227cdb16cac45e6406def7e79e1a3931c6d7606e
Julia Graves <jgraves@wesleyan.edu>
2017-08-21 12:27:41 -0400

    add drop in routes to dropin-routes.js

--- services/dropin-helper.js
+++ services/dropin-helper.js
@@ -3,7 +3,13 @@ const dropInHelper = {};
 
 dropInHelper.getDropInCenters = () => {
 	axios.get('https://data.cityofnewyork.us/resource/kjtk-8yxq.json')
-	.then((res) => console.log(res.data))
+	.then((res) => {
+		//console.log(res.data)
+		res.json({
+			message: 'ok',
+			data: res.data,
+		})
+	})
 	.catch(err => console.log(err));
 }
 
