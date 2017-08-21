871642aed2ce4349c743f3534c81de0042e4d606
Julia Graves <jgraves@wesleyan.edu>
2017-08-21 12:24:03 -0400

    add API request to dropInHelper.js

---
+++ services/dropInHelper.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const dropInHelper = {};
+
+dropInHelper.getDropInCenters = () => {
+	axios.get('https://data.cityofnewyork.us/resource/kjtk-8yxq.json')
+	.then((res) => console.log(res.data))
+	.catch(err => console.log(err));
+}
+
+module.exports = dropInHelper;
