afee9f06d3e8cbf609daf8cb0a618e42671ddef9
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-21 11:56:34 -0400

    add basic footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from react;
+
+class Footer extends Component {
+
+
+
+    render() {
+        return (
+            <footer>
+                {/* element should be floated left */}
+                <h2>© Open Arms -New York</h2>
+                {/* float right and links to specific info */}
+                <ul>
+                    <li>ABOUT</li>
+                    <li>|</li>
+                    <li>MEET THE TEAM</li>
+                    <li>|</li>
+                    <li>DONATE</li>
+                </ul>    
+            </footer>
+        )
+    }
+}
+
+export default Footer;
