2f7f764f8d9e5ac0036a3aae6c49ab18b8522b30
Julia Graves <jgraves@wesleyan.edu>
2017-08-21 16:46:29 -0400

    refactor homebase-routes.js

--- routes/homebase-routes.js
+++ routes/homebase-routes.js
@@ -11,9 +11,20 @@ homebaseRoutes.get('/', apiHelper.getHomebases, (req,res) => {
 })
 
 homebaseRoutes.get('/:id', apiHelper.getHomebases, (req,res) => {
+	let homebase = res.locals.data[req.params.id];
 	res.json({
 		message: 'ok',
-		data: res.locals.data[req.params.id],
+		data: {
+			address: homebase.address,
+			zipcode: homebase.postcode,
+			neighborhood: homebase.neighborhood,
+			lat: homebase.latitude,
+			lon: homebase.longitude,
+			phone: homebase.phone_number,
+			office: homebase.homebase_office,
+		}
+		
+
 	})
 })
 
