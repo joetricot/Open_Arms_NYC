613670261a8f7fd5ca862496637541c40f86681d
Julia Graves <jgraves@wesleyan.edu>
2017-08-22 10:44:57 -0400

    render homebase data from backend onto map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-//import L from 'leaflet';
 import {
 	Map, 
 	Marker, 
@@ -7,20 +6,49 @@ import {
 	TileLayer 
 } from 'react-leaflet';
 
-//lat and long
-const position = [40.7831,-73.9712]
+import axios from 'axios';
+
 
 class MyMap extends Component {
+	constructor() {
+		super();
+		this.state = {
+			homebaseLocations: null,
+			homebaseDataLoaded: false,
+			position: [40.7831,-73.9712]
+		}
+		this.createHomebasePopup = this.createHomebasePopup.bind(this);
+	}
+
+	componentDidMount() {
+		console.log('did mount');
+		axios.get('/homebase')
+		.then(res => {
+			console.log(res.data.data)
+			this.setState({
+				homebaseLocations: res.data.data,
+				homebaseDataLoaded: true,
+			})
+		}).catch(err => console.log(err));
+	}
+
+	createHomebasePopup(homebase) {
+		console.log(homebase);
+		return (
+			<Marker position={[homebase.latitude,homebase.longitude]} className='homebase'>
+				<Popup>
+					<span>{homebase.address}</span>
+				</Popup>
+			</Marker>
+		)
+	}
+
 	render() {
 		return(
-			<Map center={position} zoom={13} id='map'>
+			<Map center={this.state.position} zoom={13} id='map'>
 				<TileLayer  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' 
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-				<Marker position={position}>
-					<Popup>
-						<span>Haii</span>
-					</Popup>
-				</Marker>
+				{this.state.homebaseDataLoaded ? this.state.homebaseLocations.map(this.createHomebasePopup) : ''}
 			</Map>
 		)
 	}
