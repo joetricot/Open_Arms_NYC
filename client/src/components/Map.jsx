af31c3c82bae600bc64125f86b0fc15870fb6567
Julia Graves <jgraves@wesleyan.edu>
2017-08-22 10:12:36 -0400

    set up leaflet map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+//import L from 'leaflet';
+import {
+	Map, 
+	Marker, 
+	Popup, 
+	TileLayer 
+} from 'react-leaflet';
 
-class Map extends Component {
+//lat and long
+const position = [40.7831,-73.9712]
 
-
-
-    render() {
-        return (
-            <div className="map bg-danger col-md-12">
-               <img src="https://i.stack.imgur.com/T87st.png"/>
-            </div>
-        )
-    }
+class MyMap extends Component {
+	render() {
+		return(
+			<Map center={position} zoom={13} id='map'>
+				<TileLayer  url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' 
+				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+				<Marker position={position}>
+					<Popup>
+						<span>Haii</span>
+					</Popup>
+				</Marker>
+			</Map>
+		)
+	}
 }
 
-export default Map;
+export default MyMap;
