2974d3fec7f86a44cdf2db45a91d3af1762b4561
Mark Delos Reyes <makizworks@gmail.com>
2017-08-22 16:06:11 -0400

    edit header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,6 +8,13 @@ class Header extends Component {
         return (
             <header className="header bg-info col-sm-12">
                 <h3 className="col-sm-3">Open Arms NYC</h3>
+                <nav className="nav">
+                    <ul className=" list-inline col-xs-4 col-md-offset-5">
+                    <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">About</a></li>
+                    <li className="nav-item"><a className="nav-link" href="#">Contact Us</a></li>
+                    </ul>
+                </nav>
             </header>
         )
     }
