894d0648bcac43fff2706cf362605366b3e3a19a
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-22 15:07:28 -0400

    add routes move app.js mainpage to new component

---
+++ client/src/components/About.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import {
+  Link
+} from 'react-router-dom';
+
+class About extends Component {
+    render() {
+        return(
+            <div className="about">
+                <div Col xs={6} md={4}>
+                <img src="./public/lib1.jpg" />
+                </div>
+                <blockquote>
+                Your huddled masses, yearning to breathe free, The wretched refuse of your teeming shore,
+                Send these, the homeless, tempest tost to me, I lift my lamp beside the golden door.
+                </blockquote>
+            </div>
+        );
+    };    
+}
+
+export default About;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-
-
-
     render() {
         return (
             <header className="header bg-info col-sm-12">
