54afda425bb5cd00d64838b0e56039b98e48145b
Julia Graves <jgraves@wesleyan.edu>
2017-08-23 09:47:18 -0400

    create shelter controller

---
+++ controllers/shelter-controller.js
@@ -0,0 +1,95 @@
+const Shelter = require('../models/shelter');
+const shelterReview = require('../models/shelter-review');
+
+const shelterController = {};
+
+shelterController.index = (req,res) => {
+	Shelter.findAll()
+	.then(shelters => {
+		res.json({
+			message: 'ok',
+			data: shelters,
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+shelterController.show = (req,res) => {
+	Shelter.findById(req.params.id)
+	.then(shelter => {
+		res.json({
+			message: 'ok',
+			data: shelters,
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+shelterController.createDropIn = (req,res) => {
+	Shelter.create({
+		name: req.body.name,
+		category: 'dropin',
+		address: req.body.address,
+		hours: req.body.hours,
+		neighborhood: null,
+		latitude: req.body.lat,
+		longitude: req.body.lng,
+		phone: null,
+		office: null,
+	}).then(shelter => {
+			res.json({
+				message: 'ok',
+				data: shelter,
+			});  
+   }).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+shelterController.createHomebase = (req,res) => {
+	Shelter.create({
+		name: req.body.name,
+		category: 'homebase',
+		address: req.body.address,
+		hours: null,
+		neighborhood: req.body.neighborhood,
+		latitude: req.body.lat,
+		longitude: req.body.lng,
+		phone: req.body.phone,
+		office: req.body.office,
+	})
+}
+
+shelterController.addRating = (req,res) => {
+	shelterReview.create(req.params.id,req.body.rating)
+	.then(() => {
+		res.json({
+			message: 'ok',
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+shelterController.getRating = (req,res) => {
+	shelterReview.getAvgRating(req.params.id)
+	.then(rating => {
+		console.log(rating);
+		res.json({
+			message: 'ok',
+			data: rating,
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+
+module.exports = shelterController;
