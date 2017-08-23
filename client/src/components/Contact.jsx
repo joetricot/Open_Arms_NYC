73ad06a399cb6d43e47f7f1206c32831abc53493
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-23 10:31:19 -0400

    commit before merge

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,8 +4,16 @@ class Contact extends Component {
     render() {
         return(
             <div className="contactPage">
-            <textarea class="form-control" rows="6" placeholder="Ideas, Comments, Questions, or Suggestions Welcomed!"></textarea>
+                <div>
+                <h3 className="">Contact</h3><hr/>
+                <h5>Do you have questions, comments, or concerns?<br/>
+                <br/>
+                Please send us your thoughts!</h5>
+                </div>
+                <div class="input-group input-group-lg">
+            <textarea class="form-control" rows="8"  placeholder="Ideas, Comments, Questions, or Suggestions Welcomed!"></textarea>
             <button type="submit" class="btn btn-default">Submit</button>
+                </div>
             </div>
         )
     }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,16 +7,15 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer container-fluid bg-success row col-sm-12">
-                {/* element should be floated left */}
                 <h4 className="col-xs-8 col-md-3">© Open Arms -New York</h4>
-                {/* float right and links to specific info */}
-                <ul className="list-unstyled list-inline col-xs-4 col-md-offset-5">
+
+                {/* <ul className="list-unstyled list-inline col-xs-4 col-md-offset-5">
                     <li>ABOUT</li>
                     <li>|</li>
                     <li>MEET THE TEAM</li>
                     <li>|</li>
                     <li>DONATE</li>
-                </ul>    
+                </ul>     */}
             </footer>
         )
     }
