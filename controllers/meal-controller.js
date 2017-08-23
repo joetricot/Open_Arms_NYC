401e412c418b7c49167d5116f7fd0c55bbddb224
Julia Graves <jgraves@wesleyan.edu>
2017-08-23 14:41:08 -0400

    create mealController

---
+++ controllers/meal-controller.js
@@ -0,0 +1,55 @@
+const Meal = require('../models/meal');
+const mealController = {};
+
+mealController.index = (req,res) => {
+	Meal.findAll()
+	.then(meals => {
+		res.json({
+			message: 'ok',
+			data: meals,
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+mealController.show = (req,res) => {
+	Meal.findById(req.params.id)
+	.then(meal => {
+		res.json({
+			message: 'ok',
+			data: meal,
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+mealController.getRating = (req,res) => {
+	Meal.getRating(req.params.id)
+	.then(rating => {
+		res.json({
+			message: 'ok',
+			data: rating[0].avg,
+		}); 
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+mealController.addRating = (req,res) => {
+	Meal.addRating(req.params.id, req.body.rating)
+	.then(() => {
+		res.json({
+			message: 'ok',
+		});
+	}).catch(err => {
+      console.log(err);
+      res.status(500).json({ err });
+	});
+}
+
+module.exports = mealController;
