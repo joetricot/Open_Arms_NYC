5beaa2961cdb6f69baebee97c295329dbf18cdcc
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-24 11:58:16 -0400

    add job component framework

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -4,8 +4,83 @@ class Jobs extends Component {
     render() {
         return (
             <div className="jobs">
+
             <h1>Joes awesome job info that will get anyone a job in one week or your money back.</h1>
+        <div className="container">
+        <div className="row">
+            <div className="panel panel-default col-lg-12 col-lg-6">
+            <div className="panel-body">
+                <div className="media">
+                <div className="media-left">
+                <a href="#">
+                <img className="media-object" src="..." alt="..."/>
+                </a>
+                </div>
+                <div className="media-body">
+                <h4 className="media-heading">Media heading</h4>
+                ...
+                </div>
+                </div>
             </div>
+            </div>
+        </div> 
+        <div className="row">   
+            <div className="panel panel-default col-lg-12 col-lg-6 pull-right">
+            <div className="panel-body">
+                <div className="media">
+                <div className="media-right">
+                <a href="#">
+                <img className="media-object" src="..." alt="..."/>
+                </a>
+                </div>
+                <div className="media-body">
+                <h4 className="media-heading">Media heading</h4>
+                ...
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="panel panel-default col-lg-12 col-lg-6">
+            <div className="panel-body">
+                <div className="media">
+                <div className="media-left">
+                <a href="#">
+                <img className="media-object" src="..." alt="..."/>
+                </a>
+                </div>
+                <div className="media-body">
+                <h4 className="media-heading">Media heading</h4>
+                ...
+                </div>
+                </div>
+            </div>
+            </div>
+        </div> 
+        <div className="row">   
+            <div className="panel panel-default col-lg-12 col-lg-6 pull-right">
+            <div className="panel-body">
+                <div className="media">
+                <div className="media-right">
+                <a href="#">
+                <img className="media-object" src="..." alt="..."/>
+                </a>
+                </div>
+                <div className="media-body">
+                <h4 className="media-heading">Media heading</h4>
+                ...
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+
+        </div>    
+        </div>    
+            
+
         )
     }
 }
