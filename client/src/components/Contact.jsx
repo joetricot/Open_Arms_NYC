6a268c4ec1777c8dd39688195b187e8d3a5ea3a3
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-24 11:31:32 -0400

    add route fix update contacts

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -14,32 +14,61 @@ class Contact extends Component {
             <p>E-Mail:<input type="email" placeholder="email"></input></p>
             <textarea className="form-control commentsBox" rows="10"  placeholder="Ideas, Comments, Questions, or Suggestions Welcomed!"></textarea>
             <button type="submit" class="btn btn-default">Submit</button>
-                </div>
+                <hr/>
                 <div>
                 <h3>Additional Resources</h3>
-                
-                <div className="panel panel-default">
+                <div className="row">
+                <div className="panel panel-primary col-lg-4">
                     <div className="panel-heading">
-                        <h3 class="panel-title">Panel title</h3>
+                    <h3 class="panel-title">Food Stamps/ SNAP</h3>
                     </div>
                     <div className="panel-body">
-                        Panel content
+                    <a href="tel:855-692-4322">855-692-4322</a>
                     </div>
                     </div>
   
-                <div className="panel panel-default">
+                    <div className="panel panel-primary col-lg-4">
                     <div className="panel-heading">
-                        <h3 className="panel-title">Panel title</h3>
+                    <h3 class="panel-title">WIC Hotline</h3>
                     </div>
                     <div className="panel-body">
-                        Panel content
+                    <a href="tel:800-522-5006">800-522-5006</a>
                     </div>
                     </div>
-            
+
+                    <div className="panel panel-primary col-lg-4">
+                    <div className="panel-heading">
+                    <h3 class="panel-title">Food Pantries</h3>
+                    </div>
+                    <div className="panel-body">
+                    <a href="tel:866-888-8777">866-888-8777</a>
+                    </div>
+                    </div>
+                    </div>
+                
+                    <div className="panel panel-primary col-lg-12">
+                    <div className="panel-heading">
+                    <h3 className="panel-title">Homebound Delivered Senior Meals</h3>
+                    </div>
+                    <div className="panel-body col-lg-6">
+                    <a href="tel:212-222-2552">Washington Heights, Harlem: 212-222-2552</a>
+                    <hr/>
+                    <a href="tel:212-338-4333">East Harlem, Upper East Side: 212-338-4333</a>
+
+                    </div>
+
+                    <div className="panel-body col-lg-6">
+                    <a href="tel:212-281-2910">Upper West Side, Midtown West: 212-281-2910</a>
+                    <hr/>
+                    <a href="tel:212-349-2770">Midtown East, Chelsea, Village, Downtown: 212-349-2770</a>
+                    </div>
+                    </div>
                 </div>
             </div>
+        </div>
         )
     }
 }
 
 export default Contact;
+
--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Jobs extends Component {
     render() {
         return (
+            <div className="jobs">
             <h1>Joes awesome job info that will get anyone a job in one week or your money back.</h1>
+            </div>
         )
     }
 }
