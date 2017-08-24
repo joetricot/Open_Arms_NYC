50dbf77132659ad43ddccd07f75480052c32619d
Mark Delos Reyes <makizworks@gmail.com>
2017-08-24 11:52:13 -0400

    edit footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
 
-
-
     render() {
         return (
             <footer className="footer">
-                <div>
+                <section className="props">
+                    <h3>Sharing is caring!</h3>
+                    <button>Facebook</button> 
+                    <button>Twitter</button> 
+                </section>
+
+                <section className="copyright">
                     <div>&copy; 2017 Open Arms - NYC</div>
                     <div>made with ❤️ by Julia, Rana, Joe & Mark</div>
-                </div>
+                </section>
             </footer>
         )
     }
