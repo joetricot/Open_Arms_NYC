0dd63487bfb17db3ff078259f006681dd3d31adc
Rana Bhattacharyya <ranab33@gmail.com>
2017-08-25 11:58:14 -0400

    prepull changes

--- client/src/components/Jobs.jsx
+++ client/src/components/Jobs.jsx
@@ -20,15 +20,19 @@ class Jobs extends Component {
                 <div className="media-body">
                 <h4 className="media-heading"><a target="_blank" href="https://www.doe.org/programs/ready-willing-able">The Doe Fund</a></h4>
                 <h4>Affordable Housing, Education and Career Training</h4>
-                <p>
-Ready, Willing & Able is the most comprehensive transitional work program in the country. Since 1990 the program has transitioned more than 22,000 men back to mainstream society after incarceration and homelessness. At the heart of Ready, Willing & Able is paid transitional work—the opportunity to go to work and earn money from a participant’s first day in the program—complemented by transitional housing, education, career development and training, and personalized case management and social services. After 9-12 months, participants graduate from the program with the work and life skills necessary to achieve permanent self-sufficiency and become economic engines for their families and in their communities.</p>
-<br/>
-<p>The Doe Fund
-345 E 102nd St #305
-New York NY 10029
+                <p className="truncate">Ready, Willing & Able is the most comprehensive transitional work program in the country. Since 1990 the program has 
+                transitioned more than 22,000 men back to mainstream society after incarceration and homelessness. At the heart of Ready,
+                Willing & Able is paid transitional work—the opportunity to go to work and earn money from a participant’s first day in 
+                the program—complemented by transitional housing, education, career development and training, and personalized case 
+                management and social services. After 9-12 months, participants graduate from the program with the work and life skills 
+                necessary to achieve permanent self-sufficiency and become economic engines for their families and in their communities.</p>
+                <br/>
+                <p>The Doe Fund
+                345 E 102nd St #305
+                New York NY 10029
 
-Email: info@doe.org
-Phone: (212) 628-5207</p>
+                Email: info@doe.org
+                Phone: (212) 628-5207</p>
                 </div>
                 </div>
             </div>
@@ -44,12 +48,15 @@ Phone: (212) 628-5207</p>
                 </a>
                 </div>
                 <div className="media-body">
-                <h4 className="media-heading"><a target="_blank" href="http://www.coalitionforthehomeless.org/our-programs/first-step-job-training/first-step/">First Step Job Training Program</a></h4> <h4>First Step helps homeless and low-income <b>women</b> gain the skills, experience and confidence needed to find living-wage jobs.</h4>
-                <p>The Coalition’s First Step Job Training Program was launched in 1991 to give homeless and low-income women the training, social support, education and work experience they need to overcome those obstacles, empowering them to build a better life for themselves and their families.
+                <h4 className="media-heading"><a target="_blank" href="http://www.coalitionforthehomeless.org/our-programs/first-step-job-training/first-step/">First Step Job Training Program</a></h4> 
+                <h4>First Step helps homeless and low-income <b>women</b> gain the skills, experience and confidence needed to find living-wage jobs.</h4>
+                <p className="collapse">The Coalition’s First Step Job Training Program was launched in 1991 to give homeless and low-income women the training, social support, 
+                education and work experience they need to overcome those obstacles, empowering them to build a better life for themselves and their families.
 
-First Step offers six 14-week classes serving roughly 140 women per year. Our innovative curriculum includes over 100 hours of computer instruction, along with literacy workshops, communication and interpersonal skills development, and other hands-on activities that give students a thorough and practical understanding of the job market and workplace. First Step also places students in internships with major corporations or non-profit organizations, and offers ongoing mentoring by experienced professionals.</p>
-<br />
-<p>If you have questions about First Step or if you would like to sign up for an Informational Session, please call 212-776-2074.</p>
+                First Step offers six 14-week classes serving roughly 140 women per year. Our innovative curriculum includes over 100 hours of computer 
+                instruction, along with literacy workshops, communication and interpersonal skills development, and other hands-on activities that give students a thorough and practical understanding of the job market and workplace. First Step also places students in internships with major corporations or non-profit organizations, and offers ongoing mentoring by experienced professionals.</p>
+                <br />
+                <p>If you have questions about First Step or if you would like to sign up for an Informational Session, please call 212-776-2074.</p>
                 </div>
                 </div>
             </div>
@@ -66,14 +73,21 @@ First Step offers six 14-week classes serving roughly 140 women per year. Our in
                 </div>
                 <div className="media-body">
                 <h4 className="media-heading"><a target="_blank" href="http://www.acenewyork.org/our-programs/">ACE Programs for the Homeless</a></h4>
-                <p>
-Our 4-6 month vocational rehabilitation program, Project Comeback, provides recovering homeless individuals with the skills, support services and motivation they need to obtain and sustain full-time employment and economic independence. Participants receive job-readiness training, supported work experience (providing maintenance services to New York City streets and parks), Adult Basic Education, supplemental skills training (Occupational Safety & Health Administration [OSHA]; Food Protection) and vocational support services, including life skills development and job search assistance. Program activities take place seven days each week year-round, with each participant attending five days weekly, graduating from the program once he/she finds full-time employment.</p>
-<br/>
-<p>ACE Programs for the Homeless
-30-30 Northern Blvd., Ste. B100
-Long Island City, NY 11101
-P: 212-274-0550
-F: 646-613-8276</p>
+                <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#jobInfo">Simple collapsible</button>
+
+                <p className="collapse" id="jobInfo">
+                Our 4-6 month vocational rehabilitation program, Project Comeback, provides recovering homeless individuals with the skills, support 
+                services and motivation they need to obtain and sustain full-time employment and economic independence. Participants receive job-readiness 
+                training, supported work experience (providing maintenance services to New York City streets and parks), Adult Basic Education, supplemental 
+                skills training (Occupational Safety & Health Administration [OSHA]; Food Protection) and vocational support services, including life skills 
+                development and job search assistance. Program activities take place seven days each week year-round, with each participant attending five 
+                days weekly, graduating from the program once he/she finds full-time employment.</p>
+                <br/>
+                <p>ACE Programs for the Homeless
+                30-30 Northern Blvd., Ste. B100
+                Long Island City, NY 11101
+                P: 212-274-0550
+                F: 646-613-8276</p>
                 </div>
                 </div>
             </div>
@@ -91,18 +105,22 @@ F: 646-613-8276</p>
                 </a>
                 </div>
                 <div className="media-body">
-                <h4 className="media-heading">Henry Street Settlement</h4> <p>Henry Street offers an array of services for individuals seeking employment. Our <a target="_blank" href = "https://www.henrystreet.org/programs/employment/workforce-development-center/?_ga=2.225249946.1066426.1503668698-1110164242.1503498680"> Workforce Development Center (WDC)</a> provides the tools and resources needed to complete a successful job search. This program is also available for non-fluent English speakers, along with <a target="_blank" href="https://www.henrystreet.org/programs/employment/esl-programs/">ESL classes</a>.
+                <h4 className="media-heading">Henry Street Settlement</h4> <p>Henry Street offers an array of services 
+                for individuals seeking employment. Our <a target="_blank" href="https://www.henrystreet.org/programs/employment/workforce-development-center/?_ga=2.225249946.1066426.1503668698-1110164242.1503498680"> 
+                Workforce Development Center (WDC)</a> provides the tools and resources needed to complete a successful job search. This program is also available for non-fluent English speakers, along with 
+                <a target="_blank" href="https://www.henrystreet.org/programs/employment/esl-programs/">ESL classes</a>.
 
-We also offer specific employment and training services to people living in the Lillian Wald and Jacob Riis Houses as part of our <a target="_blank" href="https://www.henrystreet.org/programs/employment/jobs-plus-employment-and-training-services/?_ga=2.119966728.1066426.1503668698-1110164242.1503498680">Jobs Plus program.</a></p>
-<br/>
-<p>Lisa Tomanelli
+                We also offer specific employment and training services to people living in the Lillian Wald and Jacob 
+                Riis Houses as part of our <a target="_blank" href="https://www.henrystreet.org/programs/employment/jobs-plus-employment-and-training-services/?_ga=2.119966728.1066426.1503668698-1110164242.1503498680">Jobs Plus program.</a></p>
+                <br/>
+                <p>Lisa Tomanelli
 
-Director, Employment Services
+                Director, Employment Services
 
-Phone:
-212.478.5400
-Location:
-99 Essex Street, New York, NY</p>
+                Phone:
+                212.478.5400
+                Location:
+                99 Essex Street, New York, NY</p>
                 </div>
                 </div>
             </div>
